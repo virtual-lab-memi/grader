22c1ccefe1a2f143b2473ba4618756955cbb6204
Daniela Meneses <daniela11290@gmail.com>
2015-09-14 22:25:26 -0400

    Tracker: 88967104 - Restringir soluciones de acuerdo a la memoria definida Added configuration to run c++ code in docker sandbox.

--- config/config.global.js
+++ config/config.global.js
@@ -12,9 +12,10 @@ config.mongo.db = 'meteor';
 //Sandbox
 config.sandbox = {};
 config.sandbox.partialRunCount = 1;
-config.sandbox.root = '/home/daniela/sandbox';
-config.sandbox.problems = '/home/daniela/uploads';
+config.sandbox.root = '/home/dann/train-me/train-me-sandbox/output-data';
+config.sandbox.problems = '/home/dann/uploads';
 config.sandbox.expectedOutputFormat = config.sandbox.problems + '/problemFiles-%s-output';
-config.output = {}
+config.output = {};
 config.output.error = config.sandbox.root + '/error.log';
 config.output.default = config.sandbox.root + '/output';
+config.sandbox.envVars = 'SANDBOX_INPUT=%s\nSANDBOX_MEMORY=%s\nSANDBOX_TIME=%s';
