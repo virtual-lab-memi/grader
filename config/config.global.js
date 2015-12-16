c34a9f1fbf831f3ed852ccf787972409c8449093
Daniela Meneses <daniela.meneses@jalasoft.com>
2015-12-15 21:36:13 -0400

    Updating configuratin files for production

--- config/config.global.js
+++ config/config.global.js
@@ -6,14 +6,14 @@ config.port = 8888;
 
 //Mongo DB
 config.mongo = {};
-config.mongo.uri = process.env.MONGO_URI || 'localhost:3001';
-config.mongo.db = 'meteor';
+config.mongo.uri = process.env.MONGO_URI || 'localhost:27017';
+config.mongo.db = 'train-me';
 
 //Sandbox
 config.sandbox = {};
 config.sandbox.partialRunCount = 1;
-config.sandbox.root = '/home/dann/train-me/train-me-sandbox/output-data';
-config.sandbox.problems = '/home/dann/uploads';
+config.sandbox.root = '/root/sandbox/output-data';
+config.sandbox.problems = '/home/train-me/uploads';
 config.sandbox.expectedOutputFormat = config.sandbox.problems + '/problemFiles-%s-output';
 config.output = {};
 config.output.error = config.sandbox.root + '/error.log';
